42618a543247ebfe18d8da3f46ca1cf910d0f676
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-07-03 13:21:20 -0700

    Rename directories

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,17 @@
 
 set -ex
 
-cd benchmark-reporter
+cd benchmarks-image-service
 
-APP_URL=$(now --npm --public -e PRIVATE_KEY=$PRIVATE_KEY -e APP_ID=$APP_ID -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production" -e LOG_LEVEL=debug -T $NOW_TEAM -t $NOW_TOKEN)
-now scale $APP_URL sfo 1 -T $NOW_TEAM --token=$NOW_TOKEN
-now alias set $APP_URL $NOW_ALIAS -T $NOW_TEAM -t $NOW_TOKEN
-now rm $NOW_PROJECT --safe -y -T $NOW_TEAM -t $NOW_TOKEN || true
+# Global config file is required
+# See: https://github.com/cloudflare/wrangler/pull/225
+wrangler config dummy_value dummy_value
+
+wrangler publish --release
+
+# cd benchmark-reporter
+#
+# APP_URL=$(now --npm --public -e PRIVATE_KEY=$PRIVATE_KEY -e APP_ID=$APP_ID -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production" -e LOG_LEVEL=debug -T $NOW_TEAM -t $NOW_TOKEN)
+# now scale $APP_URL sfo 1 -T $NOW_TEAM --token=$NOW_TOKEN
+# now alias set $APP_URL $NOW_ALIAS -T $NOW_TEAM -t $NOW_TOKEN
+# now rm $NOW_PROJECT --safe -y -T $NOW_TEAM -t $NOW_TOKEN || true
