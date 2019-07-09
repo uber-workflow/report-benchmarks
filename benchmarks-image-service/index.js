5174db026b95e00be7759c35e788c54fae857341
Joseph Agnelli <jagnelli@uber.com>
2019-07-09 09:30:23 -0700

    Remove extra files

--- benchmarks-image-service/index.js
+++ benchmarks-image-service/index.js
@@ -1,4 +1,4 @@
-const { BoxPlot, ViolinPlot, computeStats } = require("./stats");
+const { BoxPlot, ViolinPlot, computeStats } = require("./vx-stats.es.js");
 const { scaleLinear } = require("@vx/scale");
 const ReactDOMServer = require("react-dom/server");
 const { Group } = require("@vx/group");
