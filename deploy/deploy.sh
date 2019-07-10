dd899397e9e0e30866834a641a348a41b21545af
Joseph Agnelli <jagnelli@uber.com>
2019-07-10 10:56:55 -0700

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,7 +12,7 @@ wrangler publish --release
 
 cd ../probot-app-report-benchmarks
 
-APP_URL=$(now --npm --public -n $NOW_PROJECT -e PRIVATE_KEY=@fusion-benchmarks-private-key -e APP_ID=$APP_ID -e WEBHOOK_SECRET=@fusion-benchmarks-webhook-secret -e NODE_ENV="production" -e LOG_LEVEL=debug -T $NOW_TEAM -t $NOW_TOKEN)
+APP_URL=$(now --npm --public -n $NOW_PROJECT -e PRIVATE_KEY=@fusion-benchmarks-private-key -e APP_ID=$APP_ID -e WEBHOOK_SECRET=@fusion-benchmarks-webhook-secret -e NODE_ENV="production" CHART_URL=$CHART_URL -e LOG_LEVEL=debug -T $NOW_TEAM -t $NOW_TOKEN)
 now scale $APP_URL sfo 1 -T $NOW_TEAM --token=$NOW_TOKEN
 now alias set $APP_URL $NOW_ALIAS -T $NOW_TEAM -t $NOW_TOKEN
 now rm $NOW_PROJECT --safe -y -T $NOW_TEAM -t $NOW_TOKEN || true
