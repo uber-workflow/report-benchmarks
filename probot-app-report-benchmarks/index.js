743d872a1c2c774722363f3f8c442f56c91cb3b9
Joseph Agnelli <jagnelli@uber.com>
2019-07-31 09:58:44 -0700

    Update comment format (#14)
    This change makes the comments take up less space on the PRs

--- probot-app-report-benchmarks/index.js
+++ probot-app-report-benchmarks/index.js
@@ -1,3 +1,4 @@
+var iqr = require("compute-iqr");
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -115,7 +116,12 @@ module.exports = app => {
   });
 
   function generateCommentBody(base_json, head_json) {
-    let imageStrings = [];
+    let imageStrings = [],
+      medianDeltas = [],
+      medianDurations = [],
+      titles = [],
+      iqrs = [];
+    let tallyIncreased = 0;
     if (base_json.version == head_json.version) {
       for (let i = 0; i < head_json.data.length; i++) {
         const chart = {
@@ -124,8 +130,22 @@ module.exports = app => {
           labels: [`head`, `base`],
           data: [head_json.data[i].time, base_json.data[i].time],
         };
+        titles.push(head_json.data[i].build);
+        let headMedian = median(head_json.data[i].time);
+        let baseMedian = median(base_json.data[i].time);
+        let deltaMedian = headMedian - baseMedian;
+        //  if the delta is larger than 10% of the median, bold it.
+        if (Math.abs(deltaMedian) > baseMedian * 0.1) {
+          medianDeltas.push(`**${deltaMedian}**`);
+          if (deltaMedian > 0) tallyIncreased++;
+          else tallyIncreased--;
+        } else {
+          medianDeltas.push(deltaMedian);
+        }
+        medianDurations.push(headMedian);
+        iqrs.push(iqr(head_json.data[i].time));
         imageStrings.push(
-          ` ![Boxplot](${process.env.CHART_URL}/?json=${encodeURIComponent(
+          ` [Boxplot](${process.env.CHART_URL}/?json=${encodeURIComponent(
             JSON.stringify(chart)
           )})`
         );
@@ -147,14 +167,62 @@ module.exports = app => {
             data.push(base_data.time);
           }
         }
+
         imageStrings.push(
-          ` ![Boxplot](${process.env.CHART_URL}/?json=${encodeURIComponent(
+          ` [Boxplot](${process.env.CHART_URL}/?json=${encodeURIComponent(
             JSON.stringify(chart)
           )})`
         );
       }
     }
-    return `${commentString}\n${imageStrings.join(" ")}`;
+    let strEffect = "";
+    if (tallyIncreased === 0) {
+      strEffect +=
+        "```diff\n#These changes had no significant impact on the build time.\n```";
+    } else {
+      strEffect += `\`\`\`diff\n${
+        tallyIncreased > 0 ? "-" : "+"
+      }These changes significantly ${
+        tallyIncreased > 0 ? "increased" : "decreased"
+      } the build time\n\`\`\``;
+    }
+    return `${commentString}\n**fusion-cli Benchmarks**\n${strEffect}
+      \n${createTableString(
+        titles,
+        ["∆ Median (ms)", "Median (head) (ms)", "IQR (head) (ms)", "Chart"],
+        [medianDeltas, medianDurations, iqrs, imageStrings]
+      )}`;
+  }
+
+  async function cliHasChanged(context, headSha, baseSha) {
+    const [headTarballHashes, baseTarballHashes] = await Promise.all([
+      context.github.checks.listForRef(
+        context.repo({
+          check_name: "Package tarball hashes",
+          status: "completed",
+          filter: "latest",
+          ref: headSha,
+        })
+      ),
+      context.github.checks.listForRef(
+        context.repo({
+          check_name: "Package tarball hashes",
+          status: "completed",
+          filter: "latest",
+          ref: baseSha,
+        })
+      ),
+    ]);
+    return (
+      headTarballHashes.data.total_count > 0 &&
+      baseTarballHashes.data.total_count > 0 &&
+      JSON.parse(headTarballHashes.data.check_runs[0].output.text).packages[
+        "fusion-cli"
+      ].shasum !==
+        JSON.parse(baseTarballHashes.data.check_runs[0].output.text).packages[
+          "fusion-cli"
+        ].shasum
+    );
   }
 
   async function cliHasChanged(context, headSha, baseSha) {
@@ -188,3 +256,35 @@ module.exports = app => {
     );
   }
 };
+
+function createTableString(topTitles, sideTitles, data) {
+  let outString =
+    "|[Help](https://docs.google.com/document/d/18TpXOEkjlfSVKfjdwJJZSe59Ju4U66sn4oH6egq1O84/edit?usp=sharing)|";
+  for (let i = 0; i < topTitles.length; i++) {
+    outString += `${topTitles[i]} |`;
+  }
+  outString += "\n|";
+  for (let i = 0; i <= topTitles.length; i++) {
+    outString += " ---------------- |";
+  }
+  outString += "\n|";
+  for (let i = 0; i < data.length; i++) {
+    outString += `${sideTitles[i]} |`;
+    for (let j = 0; j < data[i].length; j++) {
+      outString += `${data[i][j]} |`;
+    }
+    outString += "\n";
+  }
+  return outString;
+}
+
+function median(values) {
+  if (values.length === 0) return 0;
+
+  values.sort(function(a, b) {
+    return a - b;
+  });
+  var mid = values.length / 2;
+
+  return mid % 1 ? values[mid - 0.5] : (values[mid - 1] + values[mid]) / 2;
+}
